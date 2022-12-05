04269ca6ce93290480b90e7cc7d1ab36a3334d75
andrewyurasov <yurasov710@gmail.com>
2022-12-05 23:17:17 +0200

    create first template index.html

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello World')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'News list'
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 
-def test(request):
-    return HttpResponse('<h1>Hello World</h1>')
+
 
