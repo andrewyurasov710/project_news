e4abdc93bc208f4f858700526d17208276abf01a
Andrey <yurasov710@gmail.com>
2022-12-09 13:33:27 +0200

    create user tags

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -6,19 +6,15 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новин',
-        'categories': categories,
     }
     return render(request, template_name='news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     return render(request, template_name='news/category.html', context={'news': news,
-                                                                        'categories': categories,
                                                                         'category': category})
