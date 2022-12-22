baad68ac9e0e3b0f48b1fcbe1b0a6e30eb459042
Andrey <yurasov710@gmail.com>
2022-12-22 22:45:48 +0200

    create mixin class

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path(route='category/<int:category_id>/', view=get_category, name='category'),
     path(route='category/<int:category_id>/', view=NewsByCategory.as_view(), name='category'),
     # path(route='news/<int:news_id>/', view=view_news, name='view_news'),
-    path(route='news/<int:pk>/', view=ViewNews.as_view(), name='view_news'),
+    path(route='<int:pk>/', view=ViewNews.as_view(), name='view_news'),
     # path(route='news/add-news/', view=add_news, name='add_news'),
-    path(route='news/add-news/', view=CreateNews.as_view(), name='add_news'),
+    path(route='add-news/', view=CreateNews.as_view(), name='add_news'),
 ]
